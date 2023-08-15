61dd395215c70c9f28bea5b1d3309e5eb2e2d099
Forhad96 <forhadairdrop@gmail.com>
2023-08-15 22:12:08 +0600

    withdrods functonality complete with validation

--- bank-inside.js
+++ bank-inside.js
@@ -1,18 +1,28 @@
 // Selecting necessary field
 
 const depositFieldContainer = document.getElementById('deposit');
+const withdrawFieldContainer = document.getElementById('withdraw');
 
 // deposit input and button
-const depositBtn = depositFieldContainer.children[2];
 const depositField = depositFieldContainer.children[1];
+const depositBtn = depositFieldContainer.children[2];
+
+// deposit input and button
+const withdrawField = withdrawFieldContainer.children[1];
+const withdrawBtn = withdrawFieldContainer.children[2];
 
 // deposit total amount display
 const depositTotalAmount= document.getElementById("deposit_total");
 
+// withdraw total amount display
+const withdrawTotalAmount= document.getElementById("withdraw_total");
+
+
+
 //  total amount of your account display
 const totalBalance = document.getElementById('Total_balance');
 
-// deposit event
+//! deposit event button
 depositBtn.addEventListener('click',function(){
 
 // get deposit amount for deposit input field
@@ -52,4 +62,45 @@ totalBalance.innerText = newTotalBalance;
 
 
     
+})
+
+//! withdraw event button
+
+withdrawBtn.addEventListener("click",function(){
+// get withdraw amount for deposit input field
+const newWithdrawAmountString = withdrawField.value;
+const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+
+//after deposit clear  depositField
+withdrawField.value = '' 
+withdrawField.focus()
+
+// check input number or string
+if(isNaN(newWithdrawAmount)){
+    alert('Please Input Valid Number!!!');
+    return;
+}
+
+// update withdraw amount 
+const previousWithdrawTotalString = withdrawTotalAmount.innerText;
+const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+
+const newWithdrawTotal =previousWithdrawTotal + newWithdrawAmount;
+withdrawTotalAmount.innerText = newWithdrawTotal;
+
+// update Total balance
+const previousTotalBalanceString = totalBalance.innerText;
+const previousTotalBalance = parseFloat(previousTotalBalanceString);
+
+if(previousTotalBalance < newWithdrawAmount){
+    alert("Your haven't enough money!!!");
+    return;
+}
+
+const newTotalBalance = previousTotalBalance - newWithdrawAmount;
+
+totalBalance.innerText = newTotalBalance;
+
+
+
 })
